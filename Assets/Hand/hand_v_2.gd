a49f2bba9818a715621618dbe7d432077421b357
wizzysensei <wb.overtoom@gmail.com>
2025-01-25 21:42:30 +0100

    set mouse cursur invisble during main game

--- Assets/Hand/hand_v_2.gd
+++ Assets/Hand/hand_v_2.gd
@@ -13,6 +13,7 @@ var lerped_hand_speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	pass # Replace with function body.
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
